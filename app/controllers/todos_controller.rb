12208f0b1549d98558d5e8e38fc6775a39b70a0c
Emerson Fernandes de Freitas <mersonsi@gmail.com>
2017-04-06 15:49:59 -0300

    Removendo redundancias e refatorando o código

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,5 @@
 class TodosController < ApplicationController
-
+  before_action :set_todo, only: [:edit, :update, :show, :destroy]
   def index
     @todos = Todo.all
   end
@@ -19,15 +19,12 @@ class TodosController < ApplicationController
   end
 
   def show
-    @todo = Todo.find(params[:id])
   end
 
   def edit
-    @todo = Todo.find(params[:id])
   end
 
   def update
-    @todo = Todo.find(params[:id])
     if @todo.update(todo_params)
       flash[:notice] = "Todo was successfully updated"
       redirect_to todo_path(@todo)
@@ -37,13 +34,16 @@ class TodosController < ApplicationController
   end
 
   def destroy
-    @todo = Todo.find(params[:id])
     @todo.destroy
     flash[:notice] = "Todo was successfully deleted"
     redirect_to todos_path
   end
 
   private
+
+    def set_todo
+      @todo = Todo.find(params[:id])
+    end
 
     def todo_params
       params.require(:todo).permit(:name, :description)
